99c5f1031696c45ff02aac6ff0d0a1c29d3a1d0d
Chia Chun, Lin <chiachunlin526@gmail.com>
2024-11-20 12:15:57 +0800

    go back

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
-    const mobileMenu = document.getElementById("mobile-menu");
-    mobileMenu.classList.toggle("show");
-  }
-  
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
