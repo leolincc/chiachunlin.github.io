a2c9d1af69d6d49d045ef7088a7eb6b52a4c97e7
Chia Chun, Lin <chiachunlin526@gmail.com>
2024-11-20 11:42:23 +0800

    1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
+/* <script> */
+    function scrollToTop() {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+// </script>
